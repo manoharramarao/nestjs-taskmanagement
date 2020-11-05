38479b60bc9eb62504b3138980bc4eff2e1b8ce2
Manohar <mramarao@deloitte.com>
2020-11-05 08:08:28 +0530

    implemented GetUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, UseGuards, Req, ValidationPipe } from '@nestjs/
 import { AuthCredsDto } from './dto/auth-creds.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './user.entity';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -22,7 +24,7 @@ export class AuthController {
 
     @Post('/test')
     @UseGuards(AuthGuard())
-    test(@Req() req){
-        console.log(req);
+    test(@GetUser() user: User){
+        console.log(user);
     }
 }
