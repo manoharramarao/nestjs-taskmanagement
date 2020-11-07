7e9b61ba54b27052089c4ed248d641ae04b14484
Manohar <mramarao@deloitte.com>
2020-11-07 08:00:43 +0530

    updated jwt expiry, token to be picked form config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,6 +5,7 @@ import {Strategy, ExtractJwt} from 'passport-jwt';
 import { jwtPayload } from '../../dist/auth/jwt-payload.interface';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
@@ -15,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy){
     ){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
         });
     }
 
